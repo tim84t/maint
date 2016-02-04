8f2eb0770ccc71e9f701a7ecbbc1e4bb9168626f
Tim <tantingzhao@qtone.cn>
2016-02-04 17:59:26 +0800

    init

---
+++ web/src/main/java/com/qt/maint/controller/IndexController.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.qt.maint.controller;
+
+import com.qt.maint.sys.resource.entity.tmp.Menu;
+import com.qt.maint.sys.resource.service.ResourceService;
+import com.qt.maint.sys.user.entity.User;
+import com.qt.maint.sys.user.web.bind.annotation.CurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-1-18 下午10:15
+ * <p>Version: 1.0
+ */
+@Controller("adminIndexController")
+@RequestMapping("/admin")
+public class IndexController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+
+
+    @RequestMapping(value = {"/{index:index;?.*}"}) //spring3.2.2 bug see  http://jinnianshilongnian.iteye.com/blog/1831408
+    public String index(@CurrentUser User user, Model model) {
+
+        List<Menu> menus = resourceService.findMenus(user);
+        model.addAttribute("menus", menus);
+
+//        pushApi.offline(user.getId());
+
+        return "admin/index/index";
+    }
+
+
+    @RequestMapping(value = "/welcome")
+    public String welcome(@CurrentUser User loginUser, Model model) {
+
+        return "admin/index/welcome";
+    }
+
+}
