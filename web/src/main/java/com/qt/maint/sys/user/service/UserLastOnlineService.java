de76df7fa137488630e6e55fa4c20eacbfc6657a
Tim <tantingzhao@qtone.cn>
2016-02-06 23:18:59 +0800

    init

---
+++ web/src/main/java/com/qt/maint/sys/user/service/UserLastOnlineService.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.qt.maint.sys.user.service;
+
+import com.sishuok.es.common.service.BaseService;
+import com.qt.maint.sys.user.entity.UserLastOnline;
+import com.qt.maint.sys.user.repository.UserLastOnlineRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-2-4 下午3:01
+ * <p>Version: 1.0
+ */
+@Service
+public class UserLastOnlineService extends BaseService<UserLastOnline, Long> {
+
+    private UserLastOnlineRepository getUserLastOnlineRepository() {
+        return (UserLastOnlineRepository) baseRepository;
+    }
+
+
+    public UserLastOnline findByUserId(Long userId) {
+        return getUserLastOnlineRepository().findByUserId(userId);
+    }
+
+    public void lastOnline(UserLastOnline lastOnline) {
+        UserLastOnline dbLastOnline = findByUserId(lastOnline.getUserId());
+
+        if (dbLastOnline == null) {
+            dbLastOnline = lastOnline;
+        } else {
+            UserLastOnline.merge(lastOnline, dbLastOnline);
+        }
+        dbLastOnline.incLoginCount();
+        dbLastOnline.incTotalOnlineTime();
+        //相对于save or update
+        save(dbLastOnline);
+    }
+}
---
+++ web/src/main/java/com/qt/maint/sys/user/web/controller/UserLastOnlineController.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.qt.maint.sys.user.web.controller;
+
+import com.sishuok.es.common.web.controller.BaseCRUDController;
+import com.qt.maint.sys.user.entity.UserLastOnline;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-1-28 下午4:29
+ * <p>Version: 1.0
+ */
+@Controller
+@RequestMapping(value = "/admin/sys/user/lastOnline")
+public class UserLastOnlineController extends BaseCRUDController<UserLastOnline, Long> {
+
+    public UserLastOnlineController() {
+        setResourceIdentity("sys:userLastOnline");
+    }
+}
