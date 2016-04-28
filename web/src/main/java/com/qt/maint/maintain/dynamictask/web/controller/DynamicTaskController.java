9b4db6c2c86a804f9077db1f6305f59fc6640134
Tim <tantingzhao@qtone.cn>
2016-04-28 10:44:53 +0800

    1

--- web/src/main/java/com/qt/maint/maintain/dynamictask/web/controller/DynamicTaskController.java
+++ web/src/main/java/com/qt/maint/maintain/dynamictask/web/controller/DynamicTaskController.java
@@ -5,12 +5,12 @@
  */
 package com.qt.maint.maintain.dynamictask.web.controller;
 
-import com.google.common.collect.Lists;
+//import com.google.common.collect.Lists;
 import com.qt.maint.common.Constants;
 import com.qt.maint.common.web.controller.BaseCRUDController;
 import com.qt.maint.maintain.dynamictask.entity.TaskDefinition;
 import com.qt.maint.maintain.dynamictask.service.DynamicTaskApi;
-import org.apache.commons.collections4.CollectionUtils;
+//import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +18,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 import javax.validation.Valid;
-import java.util.List;
+//import java.util.List;
 
 /**
  * <p>User: Zhang Kaitao
